c401f3ec3bc3c957f044599782b93c1528a5468e
Bhumi Sahebrao Zalte <111906536+BhumiZalte12@users.noreply.github.com>
2024-11-28 11:04:07 +0530

    Database updated

--- next.config.js
+++ next.config.js
@@ -1,10 +1,14 @@
-/**
- * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
- * for Docker builds.
- */
-import "./src/env.js";
+import path from 'path';
 
-/** @type {import("next").NextConfig} */
-const config = {};
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve('src'), // Update alias for '@'
+    };
+    return config;
+  },
+};
 
-export default config;
+export default nextConfig;
